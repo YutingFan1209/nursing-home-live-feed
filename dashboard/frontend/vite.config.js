deb4882d9df30b4946305fb4bbf7c50d6f27e814
YutingFan1209 <yf1437@nyu.edu>
2026-06-08 11:03:09 -0400

    Fix base path for GitHub Pages

--- dashboard/frontend/vite.config.js
+++ dashboard/frontend/vite.config.js
@@ -1,12 +1,13 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/nursing-home-live-feed/',
   server: {
     port: 3000,
     proxy: {
-      "/api": "http://localhost:8000",
+      '/api': 'http://localhost:8000',
     },
   },
-});
+})
