6cedb95cb9c9342c10184c2e715aa9c51c824599
YutingFan1209 <yf1437@nyu.edu>
2026-06-23 18:51:23 -0400

    fix: re-enable ENABLE_NY_PLAYWRIGHT (was accidentally set to False)

--- scraper/ucc.py
+++ scraper/ucc.py
@@ -16,7 +16,7 @@ logger = logging.getLogger(__name__)
 
 ENABLE_NJ_AUTOMATION = False
 ENABLE_MAINE_AUTOMATION = False
-ENABLE_NY_PLAYWRIGHT = False
+ENABLE_NY_PLAYWRIGHT = True
 ENABLE_NJ_PLAYWRIGHT = False
 
 
